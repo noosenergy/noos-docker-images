80f9d78d45b4bc23779ce56b8f36648a549abc9d
chribag <christophe.bagot@gmail.com>
2021-07-28 14:18:32 +0200

    Clean moved scripts

--- docker/manualcron/scripts/sync_mf_nwp_models.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# syncing data from s3 bucket of https://mf-models-on-aws.org/
-aws s3 sync s3://mf-nwp-models/arpege-europe s3://noos-prod-neptune-services/Raw/METEOFRANCE/arpege-europe --exclude "*" --include "*TMP/2m/*" --exclude "v1/*" --include "static/*"
-aws s3 sync s3://mf-nwp-models/arome-france-hd s3://noos-prod-neptune-services/Raw/METEOFRANCE/arome-france-hd --exclude "*" --include "*TMP/2m/*" --exclude "v1/*" --include "static/*"

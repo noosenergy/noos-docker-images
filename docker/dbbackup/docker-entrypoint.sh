09eeb9afafd69c61c6626cc4c19f9b71ac82c96b
pieeri <eric@rubicubix.co.uk>
2021-11-22 21:41:20 +0000

    Upgrade AWSCLI and Postgresql-client versions

--- docker/dbbackup/docker-entrypoint.sh
+++ docker/dbbackup/docker-entrypoint.sh
@@ -4,16 +4,6 @@
 set -e
 
 # Check env variables aren't missing
-if [ -z "${AWS_ACCESS_KEY_ID}" ]; then
-  echo "$(date) - missing AWS_ACCESS_KEY_ID environment variable."
-  exit 1
-fi
-
-if [ -z "${AWS_SECRET_ACCESS_KEY}" ]; then
-  echo "$(date) - missing AWS_SECRET_ACCESS_KEY environment variable."
-  exit 1
-fi
-
 if [ -z "${DATABASE_PASSWORD}" ]; then
   echo "$(date) - missing DATABASE_PASSWORD environment variable."
   exit 1
@@ -34,13 +24,13 @@ export PGPASSWORD=$DATABASE_PASSWORD
 
 case "$1" in
 
-    backup_to_s3)
+    backup-to-s3)
         echo "$(date) - dumping ${PGDB} from host: ${PGHOST}"
         TMP_FILE="$(date +%Y-%m-%d).sql.gz"
         pg_dump -d $PGDB -p $PGPORT -U $PGUSER -h $PGHOST | gzip > $TMP_FILE
 
         echo "$(date) - uploading file ${TMP_FILE} to ${DATABASE_BUCKET}"
-        S3_URL="s3://${DATABASE_BUCKET}/${PGDB}/${TMP_FILE}"
+        S3_URL="${DATABASE_BUCKET}/${PGDB}/${TMP_FILE}"
         aws s3 cp $TMP_FILE $S3_URL > /dev/null
         ;;
 

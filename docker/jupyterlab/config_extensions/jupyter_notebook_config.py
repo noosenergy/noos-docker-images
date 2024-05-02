92e936e3198b7b6965bb9fea4f1a101ce09a6c38
TheTimb <timothee.brugiere@gmail.com>
2024-05-02 17:28:36 +0200

    Add templates extension to the hub

---
+++ docker/jupyterlab/config_extensions/jupyter_notebook_config.py
@@ -0,0 +1,14 @@
+#------------------------------------------------------------------------------
+# Jupyter-templates configuration
+#------------------------------------------------------------------------------
+# more info:
+# https://github.com/finos/jupyterlab_templates
+
+c = get_config()
+
+# Directory to save templates files
+c.JupyterLabTemplates.template_dirs = \
+    ['/home/jovyan/.jupyter/lab/user-settings/jupyterlab_templates']
+# Do not load default template examples
+c.JupyterLabTemplates.include_default = False
+c.JupyterLabTemplates.include_core_paths = False
